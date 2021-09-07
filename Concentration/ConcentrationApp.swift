a689c04177fd78034fd04767d36ecd8e84f245be
Trevor Schmidt <trevorjschmidt97@gmail.com>
2021-09-07 09:31:42 -0600

    Initial Commit

---
+++ Concentration/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Concentration
+//
+//  Created by Trevor Schmidt on 9/7/21.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Concentration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Concentration
+//
+//  Created by Trevor Schmidt on 9/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
